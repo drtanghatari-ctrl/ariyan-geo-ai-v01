4c0df6abef2df1effe27d25380f0e350ea42901d
drtanghatari-ctrl <dr.tanghatari@gmail.com>
2026-08-23 02:32:07 +0330

    Update build.gradle.kts

--- ARIYAN_GEO_AI/app/build.gradle.kts
+++ ARIYAN_GEO_AI/app/build.gradle.kts
@@ -72,6 +72,7 @@ chaquopy {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
